9e2eea12d3cef12030ad81452d78ae1f81afdaff
Matheus <mt.schreiber01@gmail.com>
2022-12-06 23:00:31 -0300

    scanner

---
+++ exc 10/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+  public static void main(String args[]){
+    System.out.println("Write");
+    Scanner s = new Scanner(System.in);
+
+    int n = Integer.parseInt(s.nextLine());
+
+    System.out.println("NUMBER: " + n);
+  }
+}
